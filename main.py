431049a5a2da331930d5863a708fcec73e99944d
Darknessoooo <83164487+Darknessoooo@users.noreply.github.com>
2022-04-07 09:36:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ mp_Hads = mp.solutions.hands
 hands = mp_Hads.Hands(
     model_complexity= 0,
     min_detection_confidence= 0.5,
-    min_tracking_confidence=0.5)
+    min_tracking_confidence=0.5
+    max_num_hands=1)
 mpDraw = mp.solutions.drawing_utils
 finger_Coord = [(8 , 6) , (12 , 10) , (16 , 14), (20 , 18)]
 thumb_Coord = (4 , 2)
@@ -15,12 +16,15 @@ while cap.isOpened():
     success , image = cap.read()
     if not success:
         print('Не удалось получить изображение с веб камеры')
-        continue 
+        continue
+
+    cv2.imshow('image' , image)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
     RGB_image = cv2.cvtColor(image , cv2.COLOR_BGR2RGB)
     result = hands.process(RGB_image)
     if not result.multi_hand_landmarks:
         continue
     multiLandMarks = result.multi_hand_landmarks
     print("Руки" , multiLandMarks)
-    cv2.imshow('image' , image)
-    cv2.waitKey(0)
